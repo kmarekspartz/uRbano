32e73241ac51227c8db0c35d3e1acb4453da6eca
Kyle Marek-Spartz <kyle.marek.spartz@gmail.com>
2025-11-29 09:59:38 -0600

    Simplify GHA

---
+++ tests/testthat/test-styler.R
@@ -0,0 +1,14 @@
+# From https://gaborcsardi.org/2024/package-spell-checks/
+test_that("spelling", {
+  skip_on_cran()
+  skip_on_covr()
+
+  pkg <- test_path("../../")
+  if (!file.exists(file.path(pkg, "DESCRIPTION"))) {
+    pkg <- file.path(pkg, "00_pkg_src", .packageName)
+  }
+
+  expect_snapshot({
+    styler::style_pkg(dry = "fail")
+  })
+})
